cc10006fde252cabf4a16320f66a002bbd2c4323
DenisFuryaev <denis.furyaev@gmail.com>
2021-03-13 12:27:56 +0300

    xml serialisation doesn`t work with complex numbers

--- Lab1 UI WPF/Lab1_V2/App.xaml.cs
+++ Lab1 UI WPF/Lab1_V2/App.xaml.cs
@@ -5,16 +5,16 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using MyLibrary;
 
-namespace Lab1_V2
+namespace Lab1_V2_app
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
-		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    { 
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
 		{
 			MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
 			e.Handled = true;
